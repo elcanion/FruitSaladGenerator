8cea3bf0af93b8d1804dbeaaccd2c4e230e5530a
João Victor Araceli <joaoa@lacunasoftware.com>
2025-02-09 04:11:06 -0300

    Added a fruit model

---
+++ FruitSaladGenerator/Model/Fruit.cs
@@ -0,0 +1,12 @@
+﻿namespace FruitSaladGenerator.Model
+{
+    public class Fruit
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Sugar { get; set; }
+        public int Calories { get; set; }
+        public double Fat { get; set; }
+        public int Protein { get; set; }
+    }
+}
