c590664c44409b18559c4f86c00824a96e83c589
João Victor Araceli <joaoa@lacunasoftware.com>
2025-02-09 04:08:21 -0300

    Added a basic controller for fruit salads

---
+++ FruitSaladGenerator/Controllers/FruitSaladController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FruitSaladGenerator.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class FruitSaladController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
